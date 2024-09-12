ee6333c08c70d8c911b8cbf35ff10b78c95f8abe
Yone <zhiyu@live.cn>
2024-09-12 09:17:41 +0800

    Update defaults.ts
    Signed-off-by: Yone <zhiyu@live.cn>

--- bin/defaults.ts
+++ bin/defaults.ts
@@ -25,7 +25,7 @@ export const DEFAULT_PAKE_OPTIONS: PakeCliOptions = {
 export const DEFAULT_DEV_PAKE_OPTIONS: PakeCliOptions & { url: string } = {
   ...DEFAULT_PAKE_OPTIONS,
   url: 'https://emo.xzy.one',
-  name: 'WeRead',
+  name: 'Xzy Emos',
   safeDomain: ['emo.xzy.one'],
   hideTitleBar: true,
 };
